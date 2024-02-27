40fbd36c661004d8e4866c617adbaf2f6740bff4
Jakub <jchrobakk@gmail.com>
2024-02-27 19:53:17 +0100

    fix: auth guard does not redirect when on login page

--- src/lib/hooks/auth-guard.ts
+++ src/lib/hooks/auth-guard.ts
@@ -3,13 +3,13 @@ import { redirect, type Handle } from '@sveltejs/kit';
 export const authGuard: Handle = async ({ event, resolve }) => {
 	const authenticated = !!(await event.locals.getSession());
 	const accessingProtectedPage = event.route.id?.startsWith('/(protected)/');
-	const accessingUnprotectedPage = event.route.id?.startsWith('/(unprotected)/');
+	const acessingMainPage = event.route.id === '/';
 
 	if (!authenticated && accessingProtectedPage) {
 		redirect(301, '/');
 	}
 
-	if (authenticated && accessingUnprotectedPage) {
+	if (authenticated && acessingMainPage) {
 		redirect(301, '/dashboard');
 	}
 
