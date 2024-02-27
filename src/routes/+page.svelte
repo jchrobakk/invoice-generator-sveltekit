fcd1c36528bed752c2ae1adc49bad60a4d55e531
Jakub <jchrobakk@gmail.com>
2024-02-27 19:54:02 +0100

    feat(auth): github oauth login

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,16 @@
+<script lang="ts">
+	export let data;
+	let { supabase } = data;
+	$: ({ supabase } = data);
+	import { Button } from '$lib/components/ui/button';
+
+	const handleLoginClick = async () => {
+		supabase.auth.signInWithOAuth({
+			provider: 'github'
+		});
+	};
+</script>
+
 <h1>Login</h1>
+
+<Button on:click={handleLoginClick}>Login with GitHub</Button>
